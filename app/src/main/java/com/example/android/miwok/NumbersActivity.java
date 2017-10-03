8dccc796f152f9b0e1e1a020b56983c8496227e3
kostikov <kostikov.roman@gmail.com>
2017-10-03 21:50:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.miwok;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+
+        for (int i = 0; i < words.size(); i++) {
+            TextView textView = new TextView(this);
+            textView.setText(words.get(i));
+            rootView.addView(textView);
+        }
+
+
+//        for (int i = 0; i < words.size(); i++) {
+//            Log.v("ArrayList", "Element " + i + " equals " + words.get(i));
+//        }
+
+    }
+}
